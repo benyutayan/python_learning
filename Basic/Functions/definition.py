3d9da67ffb952d1a700bb2b0f95c4dd8a33654d6
chong ma <38223386+benyutayan@users.noreply.github.com>
2019-07-04 21:56:31 +0800

    Update definition.py

--- Basic/Functions/definition.py
+++ Basic/Functions/definition.py
@@ -1,1 +1,36 @@
 
+def my_abs(x):      # x 作为参数，
+    if x >= 0:
+        return x
+    else:
+        return -x   #return 后跟想要返回的值
+
+print(my_abs(-0.99))  # 0.99
+
+
+#空函数,什么也不做，pass
+def my_func1(x):
+    pass  
+  
+# 多个参数
+def my_func3(x,y):
+    return x*x+y*y
+
+result = my_func3(3, 4)
+print(result)          #25
+
+
+#返回值有多个
+def my_func2(x):
+    return x,x*x
+
+x,y = my_func2(3)
+print(x,y)       #3,9
+
+#多个返回值，其实是一个tuple
+u = my_func2(3)
+print(u)        #(3, 9)
+
+
+  
+
